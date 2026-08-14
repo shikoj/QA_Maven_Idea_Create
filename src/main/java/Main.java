7124d6d982468365a822a898cd484e8cd0b7aaf2
toUffie <touffie@me.com>
2026-08-14 20:46:46 +0200

    miles bonus

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketCost = 13_676;
+        int rublesPerMile = 20;
+        int miles = ticketCost / rublesPerMile;
+
+        System.out.println("Начислено бонусов: "  + miles);
+
+
+
+
+    }
+}
